de3f153ff5aec59819f1ba0e6c1f6b184c3b374d
caiordm <caiordm08@gmail.com>
2024-05-30 16:34:47 -0300

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,9 @@
+class ApplicationController < ActionController::API
+    before_action :configure_permitted_parameters, if: :devise_controller?  
+    
+    protected  
+    def configure_permitted_parameters    
+        devise_parameter_sanitizer.permit(:sign_up, keys: %i[name age last_name profession])
+        devise_parameter_sanitizer.permit(:account_update, keys: %i[name age last_name profession])
+    end
+end
