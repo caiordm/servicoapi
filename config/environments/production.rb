984d8d98ab7ee96b8f4f9cc403b741b3c36fe551
caiordm <caiordm08@gmail.com>
2024-06-18 12:57:28 -0300

    config hosts 3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
   #   "example.com",     # Allow requests from example.com
   #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
   # ]
-  config.hosts << "https://servicoapi-production.up.railway.app/"
+  config.hosts << "servicoapi-production.up.railway.app/"
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
